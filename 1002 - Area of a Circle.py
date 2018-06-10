9da9b96b378550ed218326f8444f6fcf9476550e
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-06-11 02:23:19 +0600

    Add files via upload

---
+++ 1002 - Area of a Circle.py
@@ -0,0 +1,12 @@
+"""
+
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
+            Department of CSE, City University, Bangladesh.
+
+"""
+
+r = float(input())
+a = 3.14159 * r * r
+print ('A=%.4f'%a)
